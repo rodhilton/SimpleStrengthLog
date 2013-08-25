22e13b36b9e83a73fc0616ae6e26e3f5e84af20f
rodhilton <rod.hilton@gmail.com>
2013-08-24 22:58:54 -0600

    muscle group stuff, various bugfixes

--- src/com/nomachetejuggling/ssl/ExerciseListActivity.java
+++ src/com/nomachetejuggling/ssl/ExerciseListActivity.java
@@ -1,31 +1,19 @@
 package com.nomachetejuggling.ssl;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-import com.nomachetejuggling.ssl.model.Exercise;
-import com.nomachetejuggling.ssl.model.MuscleGroups;
-
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -35,32 +23,34 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
-//TODO: tagging of exercises by muscle
-//TODO: filtering of list by muscle GROUP
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.nomachetejuggling.ssl.model.Exercise;
+import com.nomachetejuggling.ssl.model.MuscleGroups;
+
 //TODO: Long press to Edit/Delete exercise
 //TODO: Favorite (part of long press menu, later a separate star)
 //TODO: load file in a thread, it can be pretty slow
-//TODO: only store exercise name in logs
 //TODO: default file of exercises (must have favorite feature first)
 //TODO: "workout summary" feature with all of current day's stuff.  datepicker for other dates.
 //TODO: (maybe) full historical record for an exercise to see improvement.  should this be part of larger suite?
 //TODO: setting for lbs/kg ? (should it change range?)
 //TODO: replace dialog progress bar on load with one inside log area?
-
 //TODO: Context menu on long press: ["Log" (default click), "Favorite/Unfavorite" (secondary), "Edit", "Delete"]
 
 public class ExerciseListActivity extends Activity {
 	ExerciseAdapter exerciseAdapter;
-	ArrayList<Exercise> exercises;
+	ArrayList<Exercise> allExercises;
+	ArrayList<Exercise> displayExercises;
 	boolean dirty;
-	
+
 	MuscleGroups muscleGroups;
+	private String filter;
 
 	static final int ADD_EXERCISE_REQUEST = 0;
 
@@ -70,33 +60,40 @@ public class ExerciseListActivity extends Activity {
 		setContentView(R.layout.activity_exercise_list);
 
 		ListView listView = (ListView) this.findViewById(R.id.exerciseList);
-		exercises = new ArrayList<Exercise>();
+		allExercises = new ArrayList<Exercise>();
+		displayExercises = new ArrayList<Exercise>();
 
 		listView.setEmptyView(findViewById(android.R.id.empty));
 
-		exerciseAdapter = new ExerciseAdapter(this, R.layout.list_exercises, R.id.line1, exercises);
+		exerciseAdapter = new ExerciseAdapter(this, R.layout.list_exercises, R.id.line1, displayExercises);
 		listView.setAdapter(exerciseAdapter);
-		
-		listView.setOnItemClickListener(new OnItemClickListener()
-		{
+
+		listView.setOnItemClickListener(new OnItemClickListener() {
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				Exercise exercise = exercises.get(arg2);
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				Exercise exercise = allExercises.get(arg2);
 				Intent intent = new Intent(ExerciseListActivity.this, LogActivity.class);
-				intent.putExtra("exercise",exercise);
+				intent.putExtra("exercise", exercise);
 				startActivity(intent);
 			}
 		});
-		dirty=false;
+		dirty = false;
 
 		this.muscleGroups = Util.loadMuscleGroups(getResources());
+		if (savedInstanceState != null) {
+			this.filter = savedInstanceState.getString("filter");
+		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.exercise_list, menu);
+
+		MenuItem item = menu.findItem(R.id.pick_action_provider);
+		CustomActionProvider provider = (CustomActionProvider) item.getActionProvider();
+		provider.setMuscleGroups(this.muscleGroups);
+
 		return true;
 	}
 
@@ -105,7 +102,7 @@ public class ExerciseListActivity extends Activity {
 		switch (item.getItemId()) {
 		case R.id.add_exercise:
 			Intent intent = new Intent(this, AddActivity.class);
-			intent.putExtra("muscles", this.muscleGroups.getMuscles().toArray(new String[]{}));
+			intent.putExtra("muscles", this.muscleGroups.getMuscles().toArray(new String[] {}));
 			startActivityForResult(intent, ADD_EXERCISE_REQUEST);
 			return true;
 		case R.id.action_settings:
@@ -116,8 +113,7 @@ public class ExerciseListActivity extends Activity {
 	}
 
 	@Override
-	protected void onActivityResult(int requestCode, int resultCode,
-			Intent intent) {
+	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
 		super.onActivityResult(requestCode, resultCode, intent);
 		if (requestCode == ADD_EXERCISE_REQUEST) {
 			if (resultCode == RESULT_OK) {
@@ -126,25 +122,26 @@ public class ExerciseListActivity extends Activity {
 					Exercise newExercise = (Exercise) extras.getSerializable("newExercise");
 					Log.i("newExercise", newExercise.toString());
 
-					exercises.add(newExercise);
-					exerciseAdapter.notifyDataSetChanged();
-					dirty=true;
+					allExercises.add(newExercise);
+					displayExercises();
+					dirty = true;
 					saveExercises();
 				}
 			}
 		}
 	}
-	
+
 	@Override
 	public void onSaveInstanceState(Bundle savedInstanceState) {
 		super.onSaveInstanceState(savedInstanceState);
 		savedInstanceState.putSerializable("muscleGroups", this.muscleGroups);
+		savedInstanceState.putString("filter", filter);
 	}
 
 	@Override
 	public void onRestoreInstanceState(Bundle savedInstanceState) {
 		super.onRestoreInstanceState(savedInstanceState);
-		this.muscleGroups = (MuscleGroups)savedInstanceState.getSerializable("muscleGroups");
+		this.muscleGroups = (MuscleGroups) savedInstanceState.getSerializable("muscleGroups");
 	}
 
 	@Override
@@ -158,26 +155,39 @@ public class ExerciseListActivity extends Activity {
 	protected void onStart() {
 		super.onStart();
 		loadExercises();
-		exerciseAdapter.notifyDataSetChanged();
+		displayExercises();
 	}
 
 	private void saveExercises() {
-		if(dirty==false) return;
-		
+		if (dirty == false)
+			return;
+
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-		String json = gson.toJson(exercises);
+		String json = gson.toJson(allExercises);
 		File file = Util.getExerciseFile(this.getApplicationContext());
 		Log.d("IO", "Writing to " + file.getAbsolutePath() + "\n" + json);
 		try {
 			FileUtils.write(file, json, "UTF-8");
 		} catch (IOException e) {
-			Toast.makeText(
-					getApplicationContext(), 
-					getString(R.string.error_cannot_save_exercises), 
-					Toast.LENGTH_SHORT).show();
+			Toast.makeText(getApplicationContext(), getString(R.string.error_cannot_save_exercises), Toast.LENGTH_SHORT).show();
 		}
-		dirty=false;
+		dirty = false;
+	}
+
+	private void displayExercises() {
+		displayExercises.clear();
+		if (this.filter == null || this.filter.equals("All")) {
+			displayExercises.addAll(allExercises);
+		} else {
+			for (Exercise exercise : allExercises) {
+				if (muscleGroups.contains(filter, exercise)) {
+					displayExercises.add(exercise);
+				}
+			}
+		}
+		this.exerciseAdapter.notifyDataSetChanged();
+
 	}
 
 	private void loadExercises() {
@@ -189,19 +199,20 @@ public class ExerciseListActivity extends Activity {
 			Gson gson = new GsonBuilder().setPrettyPrinting().create();
 			Log.d("IO", "Start Reading from " + file.getAbsolutePath() + "\n" + json);
 
-			Type collectionType = new TypeToken<Collection<Exercise>>() {}.getType();
+			Type collectionType = new TypeToken<Collection<Exercise>>() {
+			}.getType();
 			List<Exercise> exercisesRead = gson.fromJson(json, collectionType);
 			Collections.sort(exercisesRead);
-			exercises.clear();
-			exercises.addAll(exercisesRead);
+			allExercises.clear();
+			allExercises.addAll(exercisesRead);
 		} catch (IOException e) {
-			//Ignore, a missing file is either an unmounted SD Card (unrecoverable) or a first-time run.
+			// Ignore, a missing file is either an unmounted SD Card
+			// (unrecoverable) or a first-time run.
 		}
 	}
 
 	private static class ExerciseAdapter extends ArrayAdapter<Exercise> {
-		public ExerciseAdapter(Context context, int layout, int resId,
-				List<Exercise> items) {
+		public ExerciseAdapter(Context context, int layout, int resId, List<Exercise> items) {
 			super(context, layout, resId, items);
 		}
 
@@ -209,20 +220,27 @@ public class ExerciseListActivity extends Activity {
 		public View getView(int position, View convertView, ViewGroup parent) {
 			View row = convertView;
 			if (row == null) {
-				row = LayoutInflater.from(getContext()).inflate(
-						R.layout.list_exercises, parent, false);
+				row = LayoutInflater.from(getContext()).inflate(R.layout.list_exercises, parent, false);
 			}
 			Exercise item = getItem(position);
 			TextView text = (TextView) row.findViewById(R.id.line1);
 			text.setText(item.name);
-			
-			if(item.muscles != null && item.muscles.length > 0) {
-				TextView muscleListView = (TextView) row.findViewById(R.id.muscleList);
+
+			TextView muscleListView = (TextView) row.findViewById(R.id.muscleList);
+			if (item.muscles != null && item.muscles.length > 0) {
 				muscleListView.setText(Util.join(item.muscles, ", ", ""));
 				muscleListView.setVisibility(View.VISIBLE);
+			} else {
+				muscleListView.setText("");
+				muscleListView.setVisibility(View.GONE);
 			}
 			return row;
 		}
+	}
+
+	public void selectFilter(CharSequence title) {
+		this.filter = title.toString();
+		displayExercises();
 	}
 
 }
